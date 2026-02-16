309dd26a72405df121ddc319c949981b1d316706
manansharma <ms5456@srmist.edu.in>
2026-02-16 16:14:07 +0530

    Application Entry & Welcome Message

---
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+/**
+ * Application entry point
+ * This is the first method executed by the JVM
+ * When the program starts
+ * param args command line arguments
+ */
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome checker management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialize successfully");
+    }
+}
