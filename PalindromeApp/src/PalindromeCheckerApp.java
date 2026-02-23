68004dba432aada6a81c5d522d7d37a775b80503
Manan Sharma <ms5456@srmist.edu.in>
2026-02-23 15:57:32 +0530

    Manan Added UseCase2 Hardcoded Palindrome

--- PalindromeApp/src/PalindromeCheckerApp.java
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -1,13 +1,33 @@
-/**
- * Application entry point
- * This is the first method executed by the JVM
- * When the program starts
- * param args command line arguments
- */
+// version 2.0
+// UseCase 2 Hardcoded Palindrome
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome checker management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialize successfully");
+
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        for(int i = 0; i < input.length()/2; i++) {
+
+            if(input.charAt(i) != input.charAt(input.length()-1-i)) {
+
+                isPalindrome = false;
+                break;
+
+            }
+
+        }
+
+        System.out.println("Input: " + input);
+
+        if(isPalindrome)
+            System.out.println("It is Palindrome");
+
+        else
+            System.out.println("It is not Palindrome");
+
     }
+
 }
