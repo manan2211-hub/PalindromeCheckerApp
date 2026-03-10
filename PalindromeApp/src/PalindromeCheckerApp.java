0e7830487ceb7a49176f7d11253d939c4b7fae3b
Hadi-Parvez <hadi2006parvez@gmail.com>
2026-03-10 15:19:47 +0530

    Manan Added UseCase10 Case Insensitive Palindrome

--- PalindromeApp/src/PalindromeCheckerApp.java
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -1,15 +1,21 @@
+// version 10.0
+// author Manan Sharma
+// UseCase 10 Case-Insensitive & Space-Ignored Palindrome
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String input = "A man a plan a canal Panama";
 
+        // Normalize string: remove spaces and convert to lowercase
         String normalized = input.replaceAll("\\s+", "").toLowerCase();
 
         boolean isPalindrome = true;
 
-        for(int i = 0; i < normalized.length()/2; i++){
-            if(normalized.charAt(i) != normalized.charAt(normalized.length()-1-i)){
+        for(int i = 0; i < normalized.length() / 2; i++) {
+
+            if(normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
                 isPalindrome = false;
                 break;
             }
